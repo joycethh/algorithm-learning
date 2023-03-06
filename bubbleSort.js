fb1a990aa92d2ec0c721011515744a99c43f5979
joycethh <joycethh@outlook.com>
2023-03-06 14:45:46 -0600

    add bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  let temp;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(arr[i], arr[j]);
+
+      if (arr[j] < arr[i]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+bubbleSort([2, 6, 3, 5]);
