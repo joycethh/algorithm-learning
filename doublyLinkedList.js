f10bbd6d7947c10eab18ed99993b4bf3b6997b8a
joycethh <joycethh@outlook.com>
2023-05-04 15:15:29 -0500

    define push method

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -12,4 +12,20 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    var myNewNode = new Node(val);
+    if (!this.head) {
+      this.head = myNewNode;
+      this.tail = myNewNode;
+    } else {
+      this.tail.next = myNewNode;
+      myNewNode.prev = this.tail;
+      this.tail = myNewNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+var myDoubleList = new DoublyLinkedList();
+myDoubleList.push("10");
