5d6fa54b7aaac257af06521a184372e32b26dfc5
joycethh <joycethh@outlook.com>
2023-05-04 13:04:04 -0500

    set up skelton for doubly linked list

---
+++ doublyLinkedList.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(val) {
+    this.next = null;
+    this.prev = null;
+    this.val = val;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
