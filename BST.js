a5ddc168a88943964c7c77edfa8d5647ccb054db
joycethh <joycethh@outlook.com>
2023-05-09 16:15:26 -0500

    define insert method with recrussively functions

--- BST.js
+++ BST.js
@@ -10,4 +10,34 @@ class BST {
   constructor() {
     this.root = null;
   }
+  // Inserting a node, Recurrsivly
+  insert(val) {
+    const newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    this.insertNode(this.root, newNode);
+    return this;
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.val < node.val) {
+      //goes to the left
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      //goes to the right
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
 }
+
+const bst = new BST();
