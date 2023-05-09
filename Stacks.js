5788136dad15e3d4cb5794b59d52b7d8f5b94458
joycethh <joycethh@outlook.com>
2023-05-09 11:30:56 -0500

    implement stack with SLL

---
+++ Stacks.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// we are implementing stacks with SSL,
+// for the time complexsity, I will add & remove from the beganiing,
+// as I don't want to illterate the entire link to remove from the end,
+// so actually the methods are UNSHIFT & SHIFT
+// but i will still call them PUSH & POP
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    //unshift
+    var node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      node.next = this.first;
+      this.first = node;
+    }
+    this.length++;
+    return this;
+  }
+}
