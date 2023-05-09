3f8dc08aad0017f1ff02318fe7bccab399c3b980
joycethh <joycethh@outlook.com>
2023-05-09 13:51:01 -0500

    define queque add item to the begaining method

--- Stacks.js
+++ Stacks.js
@@ -45,3 +45,23 @@ class Stack {
 }
 
 const stacks = new Stack();
+
+class queque {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    return ++this.size;
+  }
+  dequeue() {}
+}
