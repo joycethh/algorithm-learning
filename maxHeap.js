0992aef10d54e06f4ba394fdaf8aba828a4fbe14
joycethh <joycethh@outlook.com>
2023-05-19 11:46:04 -0500

    add extractMax operation, the code is long and ugly though

--- maxHeap.js
+++ maxHeap.js
@@ -23,6 +23,44 @@ class MaxHeap {
     }
     return this.values;
   }
+  extractMax() {
+    if (this.values.length === 0) return null;
+
+    [this.values[0], this.values[this.values.length - 1]] = [
+      this.values[this.values.length - 1],
+      this.values[0],
+    ];
+    const oldRoot = this.values.pop();
+
+    let parentIdx = 0;
+    let leftIdx = parentIdx * 2 + 1;
+    let rightIdx = parentIdx * 2 + 2;
+
+    while (leftIdx < this.values.length && rightIdx < this.values.length) {
+      let leftChild = this.values[leftIdx];
+      let rightChild = this.values[rightIdx];
+      let parent = this.values[parentIdx];
+
+      if (rightChild > leftChild) {
+        if (parent >= rightChild) break;
+        // [parent, rightChild] = [rightChild, parent]; /// this line only operate inside the extractMax function scope,
+        this.swap(parentIdx, rightIdx); /// need to update the values in the this.values array
+        parentIdx = rightIdx;
+      } else {
+        if (parent >= leftChild) break;
+        this.swap(parentIdx, leftIdx);
+        parentIdx = leftIdx;
+      }
+      leftIdx = parentIdx * 2 + 1;
+      rightIdx = parentIdx * 2 + 2;
+    }
+
+    return oldRoot;
+  }
+
+  swap(i, j) {
+    [this.values[i], this.values[j]] = [this.values[j], this.values[i]];
+  }
 }
 
 const maxHeap = new MaxHeap();
