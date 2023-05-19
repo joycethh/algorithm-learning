bd91b8ff88aade14c858c3bf71f5b8e6351c2c84
joycethh <joycethh@outlook.com>
2023-05-19 12:28:29 -0500

    refabricate the method

--- maxHeap.js
+++ maxHeap.js
@@ -24,42 +24,45 @@ class MaxHeap {
     return this.values;
   }
   extractMax() {
-    if (this.values.length === 0) return null;
-
-    [this.values[0], this.values[this.values.length - 1]] = [
-      this.values[this.values.length - 1],
-      this.values[0],
-    ];
-    const oldRoot = this.values.pop();
-
-    let parentIdx = 0;
-    let leftIdx = parentIdx * 2 + 1;
-    let rightIdx = parentIdx * 2 + 2;
-
-    while (leftIdx < this.values.length && rightIdx < this.values.length) {
-      let leftChild = this.values[leftIdx];
-      let rightChild = this.values[rightIdx];
-      let parent = this.values[parentIdx];
-
-      if (rightChild > leftChild) {
-        if (parent >= rightChild) break;
-        // [parent, rightChild] = [rightChild, parent]; /// this line only operate inside the extractMax function scope,
-        this.swap(parentIdx, rightIdx); /// need to update the values in the this.values array
-        parentIdx = rightIdx;
-      } else {
-        if (parent >= leftChild) break;
-        this.swap(parentIdx, leftIdx);
-        parentIdx = leftIdx;
-      }
-      leftIdx = parentIdx * 2 + 1;
-      rightIdx = parentIdx * 2 + 2;
-    }
-
-    return oldRoot;
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+    percolateDown();
+    return max;
   }
+  percolateDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
 
-  swap(i, j) {
-    [this.values[i], this.values[j]] = [this.values[j], this.values[i]];
+    while (true) {
+      let leftIdx = 2 * idx + 1;
+      let rightIdx = 2 * idx + 2;
+      let swapIdx = null;
+      let leftChild, rightChild;
+
+      if (leftIdx < length) {
+        leftChild = this.values[leftIdx];
+        if (leftChild > element) {
+          swapIdx = leftIdx;
+        }
+      }
+
+      if (rightIdx < length) {
+        rightChild = this.values[rightChild];
+        if (
+          (swapIdx === null && rightChild > element) ||
+          (swapIdx !== null && rightChild > leftChild)
+        ) {
+          swapIdx = rightIdx;
+        }
+      }
+
+      if (swapIdx === null) break;
+      this.values[idx] = this.values[swapIdx];
+      this.values[swapIdx] = element;
+      idx = swapIdx;
+    }
   }
 }
 
@@ -68,3 +71,42 @@ maxHeap.insert(18);
 maxHeap.insert(27);
 maxHeap.insert(41);
 maxHeap.insert(39);
+
+// extractMax() {
+//     if (this.values.length === 0) return null;
+
+//     [this.values[0], this.values[this.values.length - 1]] = [
+//       this.values[this.values.length - 1],
+//       this.values[0],
+//     ];
+//     const oldRoot = this.values.pop();
+
+//     let parentIdx = 0;
+//     let leftIdx = parentIdx * 2 + 1;
+//     let rightIdx = parentIdx * 2 + 2;
+
+//     while (leftIdx < this.values.length && rightIdx < this.values.length) {
+//       let leftChild = this.values[leftIdx];
+//       let rightChild = this.values[rightIdx];
+//       let parent = this.values[parentIdx];
+
+//       if (rightChild > leftChild) {
+//         if (parent >= rightChild) break;
+//         // [parent, rightChild] = [rightChild, parent]; /// this line only operate inside the extractMax function scope,
+//         this.swap(parentIdx, rightIdx); /// need to update the values in the this.values array
+//         parentIdx = rightIdx;
+//       } else {
+//         if (parent >= leftChild) break;
+//         this.swap(parentIdx, leftIdx);
+//         parentIdx = leftIdx;
+//       }
+//       leftIdx = parentIdx * 2 + 1;
+//       rightIdx = parentIdx * 2 + 2;
+//     }
+
+//     return oldRoot;
+//   }
+
+//   swap(i, j) {
+//     [this.values[i], this.values[j]] = [this.values[j], this.values[i]];
+//   }
