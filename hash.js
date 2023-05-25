09be6d6cd3a671808d4b6ea91443a8b40f9a7034
joycethh <joycethh@outlook.com>
2023-05-25 10:46:24 -0500

    add comment

--- hash.js
+++ hash.js
@@ -28,31 +28,54 @@ const hashImprove = (key, arrayLen) => {
 };
 // this function introduces prime number which will make the value data distrubuted uniformally.
 // and only loop through the maxisum 100 characters will reduce the running time
-
+//---------------------------------------------------------------------------
 // a hash table class
 class HashTable {
   constructor(size = 53) {
-    //slot size avaiable for the output value
-    this.keyMap = new Array(size);
+    // represents the number of slots avaible for storing key-value pairs in hash table
+    this.keyMap = new Array(size); //array of length 'size' that will hold the key-value pairs
   }
   _hash(key) {
     let total = 0;
     let WEIRD_PRIME = 31;
-    for (let i = 0; i < Math.min((key.length, 100)); i++) {
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
-      let value = char.charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+      let value = char.charCodeAt(0) - 96; //convert the characters to numerical values
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length; // the modulo operation with the length of the 'keyMap' array
+      //to ensure the hash values falls within the valid index range.
     }
-    return total;
+    return total; // the hash value determine the index where the key-value pair will be stored in the 'keyMap' array.
   }
-  _set(key, value) {
+  set(key, value) {
     // accepts a key and value
     // hashes the key
     // stores the key-value pair in the hash table array via Seperate Chaining
     let index = this._hash(key); //index is the hashed key, and we want to store the key-value pair at this index
     if (!this.keyMap[index]) {
-      this.keyMap[index] = []; // [ , , , , [], , , ]
+      this.keyMap[index] = []; // [ , , , , [[], []], , , ]
     }
     this.keyMap[index].push([key, value]);
   }
+  get(key) {
+    //accepts a key
+    //hashes the key
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        // loop through the array at the index to find the nested array value matches what we look for.
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1]; //return the value
+        }
+      }
+    }
+    return undefined;
+  }
 }
+
+const ht = new HashTable();
+ht.set("yellow", "#FFFF00"); //key-value pair
+ht.set("Cyan", "#00FFFF");
+ht.set("white", "#fffff");
+ht.set("black", "#00000");
+ht.set("red", "#ff000");
+ht.set("green", "#00ff00");
