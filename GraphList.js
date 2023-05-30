1d71673ef1ad01603aa4024c0cebd8e96bcca797
joycethh <joycethh@outlook.com>
2023-05-30 12:25:00 -0500

    define remove an edge function

--- GraphList.js
+++ GraphList.js
@@ -10,11 +10,20 @@ class Graph {
     this.adjacencyList[vertex1].push(vertex2);
     this.adjacencyList[vertex2].push(vertex1);
   }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((el) => el !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((el) => el !== v1);
+  }
 }
 
 const g = new Graph();
 g.addVertex("Shanghai");
 g.addVertex("Kansas City");
+g.addVertex("Detroit");
 g.addVertex("Jefferson City");
 g.addEdge("Shanghai", "Kansas City");
+g.addEdge("Shanghai", "Detroit");
+g.addEdge("Detroit", "Kansas City");
 g.addEdge("Kansas City", "Jefferson City");
+
+console.log(g.adjacencyList);
