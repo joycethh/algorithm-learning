b9a1922e79cef3a417ca1c13be437f9b407fff54
joycethh <joycethhh@gmail.com>
2023-06-26 12:07:52 -0500

    add step run result to the insertion sort

--- insertion.js
+++ insertion.js
@@ -23,6 +23,25 @@ function insertion(arr) {
   return arr;
 }
 
+function insertion(arr) {
+  //firstRun: arr(6)[5, 2, 4, 6, 1, 3]; secondRun: arr=[5, 5, 4, 6, 1, 3]; thirdRun: arr=[2, 5, 4, 6, 1, 3]
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i]; // firstRun: current = 2 as i = 1; secondRun: current = 2, as i = 1; thirdRun: current = 4, as i = 2
+    let j = i - 1; //firstRun: j= 0; secondRun: j=-1
+
+    while (j >= 0 && arr[j] > current) {
+      // firstRun: j=0, arr[j] = 5,
+      arr[j + 1] = arr[j]; //firstRun: arr[j+1], arr[1] = 5,  arr=[5, 5, 4, 6, 1, 3]
+      j--; //firstRun: j= -1;
+    }
+
+    arr[j + 1] = current; //secondRun: j+1 = 0, arr[0] = 2, arr=[2, 5, 4, 6, 1, 3]======break out of the inner loop
+  }
+
+  console.log(arr);
+  return arr;
+}
+
 insertion([5, 2, 4, 6, 1, 3]);
 
 // using for loop, exact same logic as above function
